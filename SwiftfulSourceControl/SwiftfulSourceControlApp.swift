ccc44ffc620121f408b9df2fa203564dd73d5e72
vidovicsb <unkaaasz@gmail.com>
2026-07-17 12:26:32 -0700

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by Barnabas Vidovics on 7/17/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
