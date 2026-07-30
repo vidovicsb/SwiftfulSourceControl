b856a2680a9860b34c847b2253217e16153186b9
vidovicsb <unkaaasz@gmail.com>
2026-07-30 16:09:18 -0700

    [Clean] Description of commit messages

--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -5,6 +5,27 @@
 //  Created by Barnabas Vidovics on 7/30/26.
 //
 
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG NOT IN PRODUCTION
+ [Bug] Description of bug
+ 
+ RELEASE:
+ [Release] Desription of release
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+ 
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ 
+ 
+ */
+
 import SwiftUI
 
 struct HomeView: View {
