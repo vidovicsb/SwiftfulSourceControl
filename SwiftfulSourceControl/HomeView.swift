bb84d53151289ec314b5c2af3ae81a2165bc1b15
vidovicsb <unkaaasz@gmail.com>
2026-07-30 16:35:28 -0700

    [Clean] Updates to documentation

--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -11,6 +11,8 @@
  Commit = Save ("checkpoint") on our current branch
  Stage = Prepare changes for a commit
  Stash = Save changes for later
+ Push = Send local commits to remote repo
+ Pull = Fetch remote commits to local repo
  
  COMMIT MESSAGES
  
