b4aa164e8a21a9a3b5052bed53e54de762fdb575
vidovicsb <unkaaasz@gmail.com>
2026-07-30 16:27:45 -0700

    [Feature] Extra Git documentation

--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -6,6 +6,12 @@
 //
 
 /*
+ 
+ Clone = Copying the repo locally
+ Commit = Save ("checkpoint") on our current branch
+ Stage = Prepare changes for a commit
+ Stash = Save changes for later
+ 
  COMMIT MESSAGES
  
  NEW FEATURE:
