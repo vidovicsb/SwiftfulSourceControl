a42a1f3ad505636ad3470e185a0ac805c78e3935
vidovicsb <unkaaasz@gmail.com>
2026-08-02 13:21:04 -0700

    [Clean] Update docs

--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -16,6 +16,7 @@
  Merge = Joining two different branches
  Rebase = Moving one branch on top of another branch
  Cherry Picking = Duplicating (copying) one commit from one branch to another
+ Pull Request (PR) = Request to merge branch
  
  COMMIT MESSAGES
  
