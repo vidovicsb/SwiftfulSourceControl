051fc0ba6511b6f280b0d31785c868d1bd789c3e
vidovicsb <unkaaasz@gmail.com>
2026-08-02 13:31:16 -0700

    Crappy commit message

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
                                 .foregroundStyle(.tint)
                             Text("Swiftful!!!!!!!")
                             
-                            Button("Subscribe now!") {
+                            Button("Subscribe!") {
                                 
                             }
                             
