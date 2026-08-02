fa15dc5a8328f59b2f9cff4da3bd57c25c111a0a
vidovicsb <unkaaasz@gmail.com>
2026-08-02 13:31:40 -0700

    Another commit

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
                             Image(systemName: "globe")
                                 .font(.largeTitle)
                                 .foregroundStyle(.tint)
-                            Text("Swiftful!!!!!!!")
+                            Text("Swiftful Thinking!!!!!!!")
                             
                             Button("Subscribe!") {
                                 
