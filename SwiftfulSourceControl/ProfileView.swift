6823dd78160fcc467f6f61d7df9641014b599464
vidovicsb <unkaaasz@gmail.com>
2026-08-04 14:55:07 -0700

    [Feature] Add ProfileView

---
+++ SwiftfulSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Barnabas Vidovics on 8/4/26.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Vido")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
